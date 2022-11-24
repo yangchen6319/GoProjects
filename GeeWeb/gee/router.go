ec18afc02ad869a5051669056494c06e8d72aee8
chen@berry <yangchen6319@gamil>
2022-11-24 23:06:01 +0800

    新增动态路由

--- GeeWeb/gee/router.go
+++ GeeWeb/gee/router.go
@@ -3,20 +3,52 @@ package gee
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
+	roots   map[string]*node
 	handles map[string]HandleFunc
 }
 
 func newRouter() *router {
-	return &router{handles: make(map[string]HandleFunc)}
+	return &router{
+		roots:   make(map[string]*node),
+		handles: make(map[string]HandleFunc)
+	}
 }
 
+// 解析路径pattern
+func parsePattern(pattern string) []string  {
+	patternSplit := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range patternSplit{
+		if item != ""{
+			parts = append(parts, item)
+			if item[0] == '*'{
+				break
+			}
+		}
+	}
+	return parts
+}
+
+
 func (r *router) addRoute(method string, pattern string, handle HandleFunc) {
-	log.Printf("Route %4s - %s", method, pattern)
+	parts := parsePattern(pattern)
+
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok{
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	r.handles[key] = handle
+}
+
+func (r *router) getRouter(method string, path string) (*node, map[string]string)  {
+	searchParts := parsePattern()
 }
 
 func (r *router) handle(c *Context) {
