a7cbd2663f85d9f0b3665c084a2d09f55f5db7cd
chen@berry <yangchen6319@gamil>
2022-11-27 17:12:15 +0800

    新增分组控制

--- GeeWeb/gee/gee.go
+++ GeeWeb/gee/gee.go
@@ -1,20 +1,43 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 // HandleFunc 定义了请求的处理方式
 type HandleFunc func(c *Context)
 
+// RouterGroup 实现路由分组
+type RouterGroup struct {
+	// 同组路由前缀
+	prefix string
+	// 中间件
+	middlewares []HandleFunc
+	// 父路由组
+	parent *RouterGroup
+	// 所有的路由分组共享同一个engin实例
+	engine *Engine
+}
+
 // Engine 实现了serverHttp 接口
 type Engine struct {
+	// 这里存在一个嵌入类型
+	// 将RouterGroup作为嵌入类型放入Engin
+	// 意味着engin实例可以随意调用RouterGroup的属性
+	*RouterGroup
+	// 全部采用指针形式意味着engin可以调用任意的RouterGroup实例
+	groups []*RouterGroup
 	router *router
 }
 
 // New 是Engine的构造方法
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	// 实例化一个engine, 将engine放入RouterGroup中，这样engine可以访问routergroup，routergroup也可以访问engine
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 // 开头小写，不导出该方法
@@ -22,6 +45,39 @@ func New() *Engine {
 //	key := method + "-" + pattern
 //	engine.router[key] = handler
 //}
+
+// 创建新的routerGroup并放入engine
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	// 所有group共享同一个engine的目的在这
+	engine := group.engine
+	newGroup := &RouterGroup{
+		engine: engine,
+		prefix: group.prefix + prefix,
+		// 这里说明，所有的group都是由一个父group来的
+		parent: group,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+// 以下的方法是通过路由分组的方式添加路由信息
+
+func (group *RouterGroup) addRouter(method string, comp string, handle HandleFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handle)
+}
+
+func (group *RouterGroup) GET(pattern string, handle HandleFunc) {
+	group.addRouter("GET", pattern, handle)
+}
+
+func (group *RouterGroup) POST(pattern string, handle HandleFunc) {
+	group.addRouter("POST", pattern, handle)
+}
+
+// 这里的方式是通过engine直接添加路由信息
 
 // GET 定义了添加get请求的方法
 func (engine *Engine) GET(pattern string, handle HandleFunc) {
--- GeeWeb/main.go
+++ GeeWeb/main.go
@@ -8,25 +8,39 @@ import (
 func main() {
 	// 实例化一个engine
 	e := gee.New()
+
+	v1 := e.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee<h1>")
+		})
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you`re at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := e.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.GET("/assets/*filepath", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 	e.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee<h1>")
 	})
 	e.GET("/hello", func(c *gee.Context) {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	e.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
-	})
-	e.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	e.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})
 
 	e.Run(":8080")
